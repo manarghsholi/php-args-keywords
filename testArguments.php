6b1ad0a65b122c07bdcf54fcd280a70cb37cbc8a
manarghsholi <manar.sholi@endeavorpal.com>
2023-02-08 10:42:20 +0200

    initial commit

---
+++ testArguments.php
@@ -0,0 +1,31 @@
+<?php
+
+$val=1; 
+
+function func(string $arg1, string $arg2)
+{
+    return;
+}
+
+$startTimeBeforeLoop = microtime(true);
+$result = '';
+
+// Start loop 
+for($i = 1; $i <= 99999; $i++) 
+{
+    $startTime = microtime(true);
+    func('str1', 'str2');
+    $endTime = microtime(true);
+    $executionTime = ($endTime - $startTime); 
+    $result .= "\n {$i} It takes ".$executionTime." seconds to execute the script \n";
+}
+
+$endTimeAfterLoop = microtime(true);
+$executionTimeAfterLoop = ($endTimeAfterLoop - $startTimeBeforeLoop); 
+$result2 = "\n {$i} It takes ".$executionTimeAfterLoop." seconds to execute the script after loop \n";
+
+$myfile = fopen("args.txt", "w") or die("Unable to open file!");
+fwrite($myfile, $result);
+fwrite($myfile, "\n");
+fwrite($myfile, $result2);
+fclose($myfile);
